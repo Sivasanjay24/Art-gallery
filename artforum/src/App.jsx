bacdba739f36e62d842f17fd3dd42b68f4a3f0dd
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-07-10 11:14:47 +0530

    home-layout

--- artforum/src/App.jsx
+++ artforum/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './components/home.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <h1>Art Gallery</h1>
+    <Home/>
     </>
   )
 }
