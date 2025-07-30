90d415b5a02d6c0430b48b2432643fb2a593db1e
SrilekhaV16 <srilekhatrk@gmail.com>
2025-07-30 12:24:04 +0530

    create post alter backend

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  userId: String,
-  imagePath: String, 
-}, { timestamps: true });
+const PostSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    medium: { type: String, required: true },
+    dimensions: { type: String, required: true },
+    exhibition: { type: String, required: true },
+    exhibitionDescription: { type: String, required: true },
+    userId: { type: String, required: true },
+    imagePath: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
